22a546125e6b3a6267b4ba124d627a1beba3e3a7
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-08-24 21:35:32 +0900

    一旦コメントアウト

--- app/countries/page.tsx
+++ app/countries/page.tsx
@@ -1,13 +1,12 @@
-import { BASEURL } from "../lib/utils"
-
 export default async function Index() {
-  const response = await fetch(BASEURL + "/api/country", {
-    cache: "no-store",
-  })
-  const countries = await response.json()
-  console.log(countries)
+  // const response = await fetch(BASEURL + "/api/country", {
+  //   cache: "no-store",
+  // })
+  // const countries = await response.json()
+  // console.log(countries)
 
   return (
-    <ul className="my-auto text-foreground">{JSON.stringify(countries)}</ul>
+    <></>
+    // <ul className="my-auto text-foreground">{JSON.stringify(countries)}</ul>
   )
 }
