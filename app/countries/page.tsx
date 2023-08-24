ce034c325f1c57c3e9f118f4cc0ec66d1eae34ea
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-08-24 22:13:00 +0900

    countriesの実装

--- app/countries/page.tsx
+++ app/countries/page.tsx
@@ -1,17 +1,12 @@
-"use client"
-
 import { BASEURL } from "../lib/utils"
 
 export default async function Index() {
-  console.log(BASEURL)
-  // const response = await fetch(BASEURL + "/api/country", {
-  //   cache: "no-store",
-  // })
-  // const countries = await response.json()
-  // console.log(countries)
+  const response = await fetch(BASEURL + "/api/country", {
+    cache: "no-store",
+  })
+  const countries = await response.json()
 
   return (
-    <></>
-    // <ul className="my-auto text-foreground">{JSON.stringify(countries)}</ul>
+    <ul className="my-auto text-foreground">{JSON.stringify(countries)}</ul>
   )
 }
