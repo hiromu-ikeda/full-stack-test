6ef78a1b52120fcb70ffb777d84e37358ec1b332
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-08-24 22:08:45 +0900

    BASEURLの追加

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,1 @@
-export const BASEURL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000"
+export const BASEURL = process.env.NEXT_PUBLIC_APP_URL
